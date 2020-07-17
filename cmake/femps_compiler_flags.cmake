a26fdf5e6e2d9deeb36cdd1d4251a013df7b9bc6
danholdaway <danholdaway@users.noreply.github.com>
2020-07-17 11:25:39 -0400

    deafult ecbuild flags

--- cmake/femps_compiler_flags.cmake
+++ cmake/femps_compiler_flags.cmake
@@ -1,19 +1,75 @@
-if( NOT CMAKE_BUILD_TYPE MATCHES "Debug" )
-  add_definitions( -DNDEBUG )
-endif( )
+# (C) Copyright 2020 UCAR.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 
-#######################################################################################
-# Fortran
-#######################################################################################
+# OpenMP
+# ------
 
-if( CMAKE_Fortran_COMPILER_ID MATCHES "GNU" )
-  include( compiler_flags_GNU_Fortran )
-elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Intel" )
-  include( compiler_flags_Intel_Fortran )
-elseif( CMAKE_Fortran_COMPILER_ID MATCHES "XL" )
-  include( compiler_flags_XL_Fortran )
-elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Cray" )
-  include( compiler_flags_Cray_Fortran )
+if( HAVE_OMP )
+
+  if( CMAKE_Fortran_COMPILER_ID MATCHES "Clang" )
+
+    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fopenmp")
+
+  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Cray" )
+
+    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -homp")
+
+  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "GNU" )
+
+    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fopenmp")
+
+  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Intel" )
+
+    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -qopenmp")
+
+  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "PGI" )
+
+    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -mp")
+
+  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "XL" )
+
+    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -qsmp=omp" )
+    set( CMAKE_Fortran_LINK_FLAGS "${CMAKE_Fortran_LINK_FLAGS} -qsmp=omp" )
+
+  else()
+
+    message( STATUS "Fortran compiler with ID ${CMAKE_CXX_COMPILER_ID} will be used with CMake default options")
+
+  endif()
+
 else()
-  message( STATUS "Fortran compiler with ID ${CMAKE_Fortran_COMPILER_ID} will be used with CMake default options")
+
+  if( CMAKE_Fortran_COMPILER_ID MATCHES "Clang" )
+
+    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fno-openmp")
+
+  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Cray" )
+
+    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -hnoomp")
+
+  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "GNU" )
+
+    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fno-openmp")
+
+  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Intel" )
+
+    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -qopenmp-stubs")
+
+  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "PGI" )
+
+    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS}  ")
+
+  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "XL" )
+
+    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -qsmp=noomp" )
+    set( CMAKE_Fortran_LINK_FLAGS "${CMAKE_Fortran_LINK_FLAGS} -qsmp=noomp" )
+
+  else()
+
+    message( STATUS "Fortran compiler with ID ${CMAKE_CXX_COMPILER_ID} will be used with CMake default options")
+
+  endif()
+
 endif()
